fe3fb197521959138540e6a3614f788eee7e63d5
doann221 <doann221@gmail.com>
2022-08-25 16:33:01 +0700

    update

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/wwwroot/lib/bootstrap/dist/css/bootstrap-grid.min.css" />
+    <title>Document</title>
+    <script src="https://kit.fontawesome.com/d0e5742af8.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <img src="/banner/WEB BANNER  (1).png" alt="" style="display:block ;width: 1000px;">
+    </div>
+</body>
+</html>
---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function() {
+    $('.eye').click(function() {
+        $(this).toggleClass('open')
+        $(this).children('i').toggleClass('fa-eye-slash fa-eye')
+        if ($(this).hasClass('open')) {
+            $(this).prev().attr('type', 'text');
+        } else {
+            $(this).prev().attr('type', 'password');
+        }
+    });
+});
